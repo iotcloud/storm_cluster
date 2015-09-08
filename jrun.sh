ae76341f4a43d8c49b745bbd29af2261518fe8c2
supunkamburugamuva <supun06@gmail.com>
2015-09-08 11:43:24 -0400

    adding more

--- jrun.sh
+++ jrun.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-JSTORM_DIR=jstorm
+JSTORM_DIR=jstorm-original
 
 ssh cn01 'cd ~/jstorm_cluster/zookeeper;./bin/zkServer.sh start'
 ssh cn01 'cd ~/jstorm_cluster/$JSTORM_DIR; nohup ./bin/jstorm nimbus > nimbus.out 2> nimbus.err < /dev/null &'
 
-declare -a arr=("cn06" "cn03" "cn04", "cn05")
+declare -a arr=("cn06" "cn07" "cn04", "cn05")
 for i in "${arr[@]}"
 do
    ssh $i 'cd ~/jstorm_cluster/$JSTORM_DIR; nohup ./bin/jstorm supervisor > jsupervisor.out 2> jsupervisor.err < /dev/null &'
--- kill.sh
+++ kill.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-declare -a arr=("m2" "m3" "m4")
+declare -a arr=("cn04" "cn05" "cn06", "cn07")
 for i in "${arr[@]}"
 do
    val=`ssh $i jps | grep -ie supervisor | sed 's/\([0-9]\) .*/\1/'`
    ssh $i kill $val
 done
 
-val=`ssh m1 jps | grep -ie core | sed 's/\([0-9]\) .*/\1/'`
-ssh m1 kill $val
+#val=`ssh m1 jps | grep -ie core | sed 's/\([0-9]\) .*/\1/'`
+#ssh m1 kill $val
 val=`ssh m1 jps | grep -ie nimbus | sed 's/\([0-9]\) .*/\1/'`
 ssh m1 kill $val
-ssh m1 'cd storm_cluster/zookeeper;./bin/zkServer.sh stop'
+ssh m1 'cd ~/jstorm_cluster/zookeeper;./bin/zkServer.sh stop'
 
