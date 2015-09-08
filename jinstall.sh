6d113b772fbcf6b457d621a3785149d3f932bb31
Supun Kamburugamuva <skamburu@tempest.dsc.soic.indiana.edu>
2015-09-08 14:20:53 -0400

    adding more:

--- jinstall.sh
+++ jinstall.sh
@@ -2,4 +2,5 @@
 
 JSTORM_DIR=jstorm-original
 JSTORM=~/jstorm_cluster/$JSTORM_DIR
-cp jstorm.yaml $JSTORM/conf/
+echo $JSTORMo $JSTORM
+cp jstorm.yaml $JSTORM/conf/storm.yaml
--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-declare -a arr=("cn04" "cn05" "cn06", "cn07")
+declare -a arr=("cn04" "cn05" "cn06" "cn07")
 for i in "${arr[@]}"
 do
    val=`ssh $i jps | grep -ie supervisor | sed 's/\([0-9]\) .*/\1/'`
@@ -9,7 +9,7 @@ done
 
 #val=`ssh m1 jps | grep -ie core | sed 's/\([0-9]\) .*/\1/'`
 #ssh m1 kill $val
-val=`ssh m1 jps | grep -ie nimbus | sed 's/\([0-9]\) .*/\1/'`
-ssh m1 kill $val
-ssh m1 'cd ~/jstorm_cluster/zookeeper;./bin/zkServer.sh stop'
+val=`ssh cn01 jps | grep -ie NimbusServer | sed 's/\([0-9]\) .*/\1/'`
+ssh cn01 kill $val
+ssh cn01 'cd ~/jstorm_cluster/zookeeper;./bin/zkServer.sh stop'
 
