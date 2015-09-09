56df1de953cc8eff695eab41151a35ddef8ad271
Supun <supun06@gmail.com>
2015-09-08 22:18:11 -0400

    jstorm

--- jrun.sh
+++ jrun.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-JSTORM_DIR=jstorm-original
+JSTORM_DIR=$1
 
 echo "Starting zookeeper cluster...."
 ssh cn01 'cd ~/jstorm_cluster/zookeeper;./bin/zkServer.sh start'
 echo "Starting storm cluster...."
-ssh cn01 'cd ~/jstorm_cluster/jstorm-original; nohup ./bin/jstorm nimbus > nimbus.out 2> nimbus.err < /dev/null &'
+ssh cn01 'cd ~/jstorm_cluster/$JSTORM_DIR; nohup ./bin/jstorm nimbus > nimbus.out 2> nimbus.err < /dev/null &'
 
 declare -a arr=("cn06" "cn07" "cn04" "cn05")
 for i in "${arr[@]}"
 do
-   ssh $i 'cd ~/jstorm_cluster/jstorm-original; nohup ./bin/jstorm supervisor > jsupervisor.out 2> jsupervisor.err < /dev/null &'
+   ssh $i 'cd ~/jstorm_cluster/$JSTORM_DIR; nohup ./bin/jstorm supervisor > jsupervisor.out 2> jsupervisor.err < /dev/null &'
 done
