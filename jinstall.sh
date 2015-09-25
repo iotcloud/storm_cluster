91b2cf408c40e8358ef991901bab192cf55f7339
Supun <supun06@gmail.com>
2015-09-24 21:12:10 -0400

    jstorm

--- jinstall.sh
+++ jinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+declare -a arr=("cn06" "cn07" "cn05" "cn08" "cn09" "cn10" "cn11" "cn12")
+for i in "${arr[@]}"
+do
+   ssh $i rm -rf /scratch/jstorm
+done
+
 JSTORM_DIR=$1
 JSTORM=~/jstorm_cluster/$JSTORM_DIR
 echo $JSTORM
