1000adab768c98d6dd21a9ee265d51f876cd0bdd
Supun <supun06@gmail.com>
2015-11-01 15:28:58 -0500

    jstorm

--- jrun.sh
+++ jrun.sh
@@ -7,7 +7,9 @@ ssh cn04 'cd ~/jstorm_cluster/zookeeper;./bin/zkServer.sh start'
 echo "Starting storm cluster...."
 ssh cn04 'cd ~/jstorm_cluster/'$JSTORM_DIR'; nohup ./bin/jstorm nimbus > nimbus.out 2> nimbus.err < /dev/null &'
 
-declare -a arr=("cn15" "cn07" "cn05" "cn08" "cn09" "cn10" "cn11" "cn12")
+#declare -a arr=("cn15" "cn07" "cn05" "cn08" "cn09" "cn10" "cn11" "cn12")
+#declare -a arr=("cn15" "cn07" "cn05" "cn08" "cn09" "cn10" "cn11" "cn12")
+declare -a arr=("cn15" "cn16" "cn17" "cn18")
 #declare -a arr=("cn06" "cn07" "cn05" "cn08")
 for i in "${arr[@]}"
 do
